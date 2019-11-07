312f7a1670b41ea92e61b5df2b5edd25fafec1c8
chacetrev10 <chacetrev10@gmail.com>
2019-11-06 17:13:36 -0700

    Adds user password change capibility

--- users/views.py
+++ users/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from.models import User
-from .forms import UserRegisterForm, EditProfileForm, ProfileUpdateForm
+from .forms import (
+    UserRegisterForm,
+    EditProfileForm,
+    ProfileUpdateForm,
+
+)
 
 def register(request):
     if request.method == 'POST':
@@ -29,6 +35,28 @@ def profile(request, id):
     except User.DoesNotExist:
         raise Http404('Could not find user')
     return render(request, 'users/profile.html',{'userProfile':user})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        pass_form = PasswordChangeForm(data = request.POST, user = request.user)
+        if pass_form.is_valid():
+            pass_form.save()
+            update_session_auth_hash(request, pass_form.user)
+            messages.info(request, 'Your password has been changed successfully!')
+            return redirect('profile', request.user.id)
+
+        else:
+            messages.warning(request,'Something is wrong with your form')
+            return redirect('change password')
+
+
+    else:
+        pass_form = PasswordChangeForm(user = request.user)
+        args = {'pass_form': pass_form}
+        return render(request, 'users/change_password.html', args)
+
 
 @login_required
 def edit_profile(request):
