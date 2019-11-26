01f657c96c0ffc31e0b685e510a538dd11d33093
bradleyyma <bradley.y.ma@gmail.com>
2019-11-26 11:22:59 -0700

    dunder __in method for checking category filters

--- events/views.py
+++ events/views.py
@@ -23,23 +23,9 @@ def home(request):
     events = events.order_by('event_date')
 
     #category holds which category filter
-    category = request.GET.getlist('category')
-    if category:
-        CATEG=[
-            'Basketball',
-            'Cycling',
-            'Hiking',
-            'Running',
-            'Soccer',
-            'Tennis',
-            'Other',
-            ]
-        filtered_events = Event.objects.none()
-        #filter for each category chosen
-        for i in CATEG:
-            if i in category:
-                filtered_events = filtered_events|events.filter(category = i)
-        events = filtered_events
+    categories = request.GET.getlist('category')
+    if categories:
+        events = events.filter(category__in=categories)
     return render(request, 'home.html',{'events':events, 'form':form})
 
 def event_detail(request, id):
